a3e958e3a0b5df314a80180a3cc74b5bbd545a65
longkuroko <longsteve.jobs@gmail.com>
2021-06-24 21:43:13 +0700

    first commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SkateBoard.Startup))]
+namespace SkateBoard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

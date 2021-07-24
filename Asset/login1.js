db403435c31d1432991a4a067c872bafdb08a216
LoiKise <loitan.nguyennguyen@gmail.com>
2021-07-25 00:48:58 +0700

    chatbox

--- Asset/login1.js
+++
@@ -1,7 +0,0 @@
-﻿$('.toggle').on('click', function () {
-    $('.container').stop().addClass('active');
-});
-
-$('.close').on('click', function () {
-    $('.container').stop().removeClass('active');
-});
